641154e2452b7b542ef7fd70c661053c58036b99
Serg Kolesnyk <hazard.sk@gmail.com>
2016-09-05 16:57:30 +0000

    form thanks fix to all css's

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ $(function () {
 			dataType: "json",
 			success: function () {
 				$(".please").hide();
-				$(".thanks").show().delay(3000).fadeIn(400);
+				$(".thanks").show().delay(3000).fadeIn(500);
 				var frm = document.getElementsByClass('.contact-form')[0];
 			    frm.reset();
 			}
@@ -36,38 +36,5 @@ $(function () {
 
 });
 
-
-
-// function sendForm(e){
-//     e.preventDefault();
-//     $.ajax({
-//     url: "https://formspree.io/kolesnuk_serg@komitex.net", 
-//     method: "POST",
-//     data: {
-//         first-name: $('#fname').val(),
-// 			last-name: $('#lname').val(),
-//         email: $('#email').val(),
-// 			phone: $('#phone').val(),
-// 			subject: $('#subject').val(),
-//         message: $('#msg').val()
-        
-//     },
-//     dataType: "json",
-//     success: function(){
-//         $('#thanks').html('Thank you for Submit!');
-//     }
-// });
-// }
-
-
-// $(function(){
-    
-//         $('.aside-about').hide();
-
-//         $('button').bind('click', function(){
-// 			$('.aside-about').fadeIn(3000);
-// 			});
-
-// });
 
 
