213b08c827a68f3130d0325513302cd7ca6b0bb6
Serg Kolesnyk <hazard.sk@gmail.com>
2016-09-05 09:27:10 +0000

    lg-md-sm layouts added

--- js/index.js
+++ js/index.js
@@ -1,10 +1,69 @@
-$(function(){
-    
-        $(".aside-about").hide();
+
+$(function () {
+
+	$(".aside-about").hide();
+
+	$("button").bind('click', function(){
+		$(".aside-about").fadeIn(3000);
+	});
+
+	$("#send-msg").click(sendForm);
+
+	function sendForm(e) {
+		e.preventDefault();
+		$.ajax({
+			url: "https://formspree.io/kolesnuk_serg@komitex.net", 
+			method: "POST",
+			data: {
+				fname: $("#fname").val(),
+				lname: $("#lname").val(),
+				email: $("#email").val(),
+				phone: $("#phone").val(),
+				subject: $("#subject").val(),
+				message: $("#msg").val()
+
+			},
+			dataType: "json",
+			success: function () {
+				$("#thanks").html('Thank you for Submit!');
+			}
+		});
+	}
+
 });
 
- $(function(){
-	$('button').bind('click', function(){
-		$('.aside-about').fadeIn(3000);
-	});
-});
+
+
+// function sendForm(e){
+//     e.preventDefault();
+//     $.ajax({
+//     url: "https://formspree.io/kolesnuk_serg@komitex.net", 
+//     method: "POST",
+//     data: {
+//         first-name: $('#fname').val(),
+// 			last-name: $('#lname').val(),
+//         email: $('#email').val(),
+// 			phone: $('#phone').val(),
+// 			subject: $('#subject').val(),
+//         message: $('#msg').val()
+        
+//     },
+//     dataType: "json",
+//     success: function(){
+//         $('#thanks').html('Thank you for Submit!');
+//     }
+// });
+// }
+
+
+// $(function(){
+    
+//         $('.aside-about').hide();
+
+//         $('button').bind('click', function(){
+// 			$('.aside-about').fadeIn(3000);
+// 			});
+
+// });
+
+
